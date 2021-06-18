70b788a834f9de917e710c569fb77f457bde207c
handregister <31929920+handregister@users.noreply.github.com>
2021-06-17 23:35:11 -0400

    Fixed playerName

--- ReconDrones.lua
+++ ReconDrones.lua
@@ -8,6 +8,7 @@ local droneMaxCount = 4
 local droneMaxCountAtOnce = 2
 local blueDroneCount = 0
 local redDroneCount = 0
+local spawnerName = nil
 DroneSpawned = EVENTHANDLER:New()
 DroneSpawned:HandleEvent(EVENTS.Birth)
 
@@ -21,7 +22,7 @@ Spawn_Red_UAV = SPAWN:NewWithAlias("Red UAV-Recon-FAC","Pontiac 6-1")
     :InitKeepUnitNames(true)
 	
 ----Function to actually spawn the UAV from the players nose      
-local function spawnUAV(group, rng, coalition)
+local function spawnUAV(group, rng, coalition, playerName)
 	local range = rng * 1852
 	local hdg = group:GetHeading()
 	local pos = group:GetPointVec2()	
@@ -30,9 +31,11 @@ local function spawnUAV(group, rng, coalition)
 	if coalition == 1 then
 		Spawn_Red_UAV:SpawnFromVec2(spawnVec2)
 		redDroneCount = redDroneCount + 1
+		spawnerName = playerName
 	elseif coalition == 2 then
 		Spawn_Blue_UAV:SpawnFromVec2(spawnVec2)
 		blueDroneCount = blueDroneCount + 1
+		spawnerName = playerName
 	end
 end
 
@@ -47,12 +50,12 @@ end
 
 -- this is called from the global on birth event handler
 function ReconDrones.AddMenu(playerGroup)
-	local groupName = playerGroup:GetName()
+	local playerName = playerGroup:GetPlayerName()
 	local coalitionNumber = playerGroup:GetCoalition()
 	local menuRoot = MENU_GROUP:New(playerGroup, "UAV Reconnaissance")	
-	MENU_GROUP_COMMAND:New(playerGroup, "Spawn MQ-1 UAV 1 nm away", menuRoot, spawnUAV, playerGroup, 1, coalitionNumber)
-	MENU_GROUP_COMMAND:New(playerGroup, "Spawn MQ-1 UAV 5 nm away", menuRoot, spawnUAV, playerGroup, 5, coalitionNumber)
-	MENU_GROUP_COMMAND:New(playerGroup, "Spawn MQ-1 UAV 10 nm away", menuRoot, spawnUAV, playerGroup, 10, coalitionNumber)
+	MENU_GROUP_COMMAND:New(playerGroup, "Spawn MQ-1 UAV 1 nm away", menuRoot, spawnUAV, playerGroup, 1, coalitionNumber, playerName)
+	MENU_GROUP_COMMAND:New(playerGroup, "Spawn MQ-1 UAV 5 nm away", menuRoot, spawnUAV, playerGroup, 5, coalitionNumber, playerName)
+	MENU_GROUP_COMMAND:New(playerGroup, "Spawn MQ-1 UAV 10 nm away", menuRoot, spawnUAV, playerGroup, 10, coalitionNumber, playerName)
 	MENU_GROUP_COMMAND:New(playerGroup, "UAV RECON Drones Remaining", menuRoot, function()
 		trigger.action.outTextForCoalition(coalitionNumber, "[TEAM] Has " ..getDronesRemaining(coalitionNumber).. " Remaining UAVs", 15)		
 	end)
@@ -61,13 +64,13 @@ function ReconDrones.AddMenu(playerGroup)
 end
 
 -- notify team
-function DroneSpawned:OnEventBirth(EventData)
+function DroneSpawned:OnEventBirth(EventData)	
+	env.info("DroneSpawned:OnEventBirth 1")
 	if string.find(inspect(EventData.IniDCSGroupName), "Pontiac") then 
-		if EventData.IniPlayerName then
-			local playerName = EventData.IniGroup:GetName()			
-			local coalition = EventData.IniCoalition
-			trigger.action.outTextForCoalition(coalition,"[TEAM] " ..playerName.. " called in a UAV\nYour team has "..getDronesRemaining(coalition).." remaining UAVs", 10)
-		end
+		env.info("DroneSpawned:OnEventBirth 2")						
+		local coalition = EventData.IniCoalition
+		trigger.action.outTextForCoalition(coalition,"[TEAM] " ..spawnerName.. " called in a UAV\nYour team has "..getDronesRemaining(coalition).." remaining UAVs", 10)
+		env.info("DroneSpawned:OnEventBirth 3")
 	end
 end
 
